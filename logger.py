9f2401c7588d34ddbe5c84ba9547bf2b1dd64ba0
Gabriel MERCIER <gab.mercier51@gmail.com>
2022-05-18 16:14:02 +0200

    Refractor Code

--- logger.py
+++ logger.py
@@ -6,17 +6,16 @@ def init():
                         format='%(asctime)s %(message)s',
                         filemode='w',
                         datefmt='%d-%b-%y %H:%M:%S')
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.DEBUG)
 
 def success(message):
     Write.Print("[ Success ] " + message, interval=0.005, color=Colors.green)
-    logger.info(message)
+    logging.getLogger().info(message)
 
 def error(message):
     Write.Print("[ Error ] " + message, interval=0.005, color=Colors.red)
-    logger.error(message)
+    logging.getLogger().error(message)
 
 def info(message):
     Write.Print("[ Info ] " + message, interval=0.005, color=Colors.blue)
-    logger.info(message)
+    logging.getLogger().info(message)
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import logger
 
 apikey = ""
 
-def getValue(message):
+def get_value(message):
     return Write.Input(message + " -> ", Colors.yellow_to_red, interval=0.005)
 
 if __name__ == '__main__':
     logger.init()
     load.load()
-    logger.success("Successfully log into " + getValue("Enter your api key"))
+    logger.success("Successfully log into " + get_value("Enter your api key"))
